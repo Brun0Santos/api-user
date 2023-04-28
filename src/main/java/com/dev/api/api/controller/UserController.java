770db6c161326ab19255147617e7ee82eebb5f96
Bruno Santos <bruno_st17@hotmail.com>
2023-04-27 21:15:05 -0300

    Ajustando a service

--- src/main/java/com/dev/api/api/controller/UserController.java
+++ src/main/java/com/dev/api/api/controller/UserController.java
@@ -1,17 +1,32 @@
 package com.dev.api.api.controller;
 
 import com.dev.api.api.entities.UserEntity;
+import com.dev.api.api.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    UserServices services;
+
     @GetMapping
-    public ResponseEntity<UserEntity> returnALlUsers() {
-        return ResponseEntity.ok().body(new UserEntity());
+    public ResponseEntity<List<UserEntity>> returnALlUsers() {
+        List<UserEntity> allUsers = services.getAllUsers();
+        return ResponseEntity.ok().body(allUsers);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<UserEntity> returnUserFromId(@PathVariable("id") Integer id) {
+        return services.getUserFromID(id);
     }
 }
---
+++ src/main/java/com/dev/api/api/services/UserServices.java
@@ -0,0 +1,28 @@
+package com.dev.api.api.services;
+
+
+import com.dev.api.api.entities.UserEntity;
+import com.dev.api.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServices {
+
+    @Autowired
+    UserRepository repository;
+
+    public List<UserEntity> getAllUsers() {
+        return repository.findAll();
+    }
+
+    public Optional<UserEntity> getUserFromID(Integer id) {
+        Optional<UserEntity> user = repository.findById(id);
+        user.orElseThrow();
+        return user;
+    }
+}
+
